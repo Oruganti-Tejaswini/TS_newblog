6131ef01f935434e1cb9f6ad81e499faef0d006e
Oruganti-Tejaswini <38122199+Oruganti-Tejaswini@users.noreply.github.com>
2023-08-12 18:42:00 -0400

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+    return(
+        <section className = "flex-start flex-col paddings mb-16">
+           <h1>categories</h1>
+           <h1>Posts</h1>
+           <h1>Load More</h1>
+        </section>
+    )
+}
+
+export default Home;
